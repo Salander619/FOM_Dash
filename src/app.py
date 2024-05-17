980cbdef6793c37d93d48504dd1a82b2063ae0aa
Salander619 <come.badet@laposte.net>
2024-05-17 09:30:47 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -3,7 +3,6 @@ import configparser
 import dash
 from dash import Dash, html, dcc, callback, Output, Input
 import dash_bootstrap_components as dbc
-import dash_daq as daq # pylint: disable=import-error
 
 ##############################################################################
 # Initialize the app
