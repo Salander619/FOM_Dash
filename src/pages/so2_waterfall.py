d4410ff24d373179d4309b069dad37bb4f150b23
Salander619 <come.badet@laposte.net>
2024-05-28 15:18:22 +0200

    black on so2_waterfall

--- src/pages/so2_waterfall.py
+++ src/pages/so2_waterfall.py
@@ -1,11 +1,12 @@
 """ Page of the waterfall plot """
+
 import dash
 from dash import html, dcc, callback, Output, Input
 import dash_bootstrap_components as dbc
 import plotly.graph_objects as go
 import numpy as np
 
-from config_manager import ConfigManager # pylint: disable=import-error
+from config_manager import ConfigManager  # pylint: disable=import-error
 
 ##############################################################################
 
@@ -16,51 +17,49 @@ conf_manager = ConfigManager("data/configuration.ini")
 ##############################################################################
 
 # layout of the page
-layout = html.Div([ # pylint: disable=unused-variable
-    html.H1('Waterfall plot'),
-
-    dcc.Graph(
-        id='waterfall_graph',
-        figure={
-            "layout": {
-                "height": 700,  # px
+layout = html.Div(
+    [  # pylint: disable=unused-variable
+        html.H1("Waterfall plot"),
+        dcc.Graph(
+            id="waterfall_graph",
+            figure={
+                "layout": {
+                    "height": 700,  # px
+                },
             },
-        }
-    ),
-
-    dbc.Nav(
-        [
-            html.Div(
-                dbc.NavLink(
-                    "View as notebook",
-                    href="https://nbviewer.org/github/Salander619/FOM_Dash/blob/main/src/notebooks/waterfall_plot.ipynb", # pylint: disable=line-too-long
-                    active='exact',
+        ),
+        dbc.Nav(
+            [
+                html.Div(
+                    dbc.NavLink(
+                        "View as notebook",
+                        href="https://nbviewer.org/github/Salander619/FOM_Dash/blob/main/src/notebooks/waterfall_plot.ipynb",  # pylint: disable=line-too-long
+                        active="exact",
+                    ),
                 ),
-            ),
-        ],
-        vertical=True,
-        pills=True,
-    ),
-])
+            ],
+            vertical=True,
+            pills=True,
+        ),
+    ]
+)
 
 ##############################################################################
 # Create plots
 
+
 # pylint: disable=unused-variable
-@callback(
-    Output("waterfall_graph", "figure"),
-    Input("config_noise_budget", "data")
-)
+@callback(Output("waterfall_graph", "figure"), Input("config_noise_budget", "data"))
 def update_graph(noise):
     """This function return the waterfall plot
     based on the noise config selected by the user
-    
+
     :param string data_file: path to the data
-    
-    :return figure waterfall_graph: plot snr 
+
+    :return figure waterfall_graph: plot snr
         based on redshift and total mass"""
 
-    data_file = conf_manager.get_data_file('SO2.waterfall',noise)
+    data_file = conf_manager.get_data_file("SO2.waterfall", noise)
     t = np.load(data_file, allow_pickle=True)
 
     # pylint: disable=unused-variable
