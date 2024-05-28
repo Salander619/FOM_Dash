a954d29280ed8b52892ae323458395a9bc038dc3
Salander619 <come.badet@laposte.net>
2024-05-28 15:17:58 +0200

    black on config_manager

--- src/config_manager.py
+++ src/config_manager.py
@@ -5,6 +5,7 @@ in order to provide the correct data to the application
 
 import configparser
 
+
 class ConfigManager:
     """
     Read and store the different configuration and data from the ini file
@@ -33,29 +34,33 @@ class ConfigManager:
             if config_2_check == config_name:
                 return self.config[section_name][option]
 
-        raise ValueError("""The configuration asked for is 
-                         not contained in the configuration file""")
+        raise ValueError(
+            """The configuration asked for is 
+                         not contained in the configuration file"""
+        )
 
     def is_tuple(self, input_string):
         """
-        Return if the input string is written with the tuple syntax 
+        Return if the input string is written with the tuple syntax
         (with parenthesis and a comma)
 
         :param strin input_string: the string to analyse
 
-        :return boolean: True is the input's syntax is correct 
+        :return boolean: True is the input's syntax is correct
         and False otherwise
         """
 
         size = len(input_string)
-        if (input_string.startswith("(",0,1)
-           and input_string.startswith(")",size-1,size)
-           and "," in input_string):
+        if (
+            input_string.startswith("(", 0, 1)
+            and input_string.startswith(")", size - 1, size)
+            and "," in input_string
+        ):
             return True
 
         return False
 
-    def parse_config(self,input_string):
+    def parse_config(self, input_string):
         """
         Parse the value string found in the ini file
 
@@ -67,13 +72,13 @@ class ConfigManager:
 
     # Source:
     # https://stackoverflow.com/questions/56967754/how-to-store-and-retrieve-a-dictionary-of-tuples-in-config-parser-python
-    def parse_tuple(self,input_string):
+    def parse_tuple(self, input_string):
         """
         Parse a string as a tuple
 
         :param string input_string: string to convert as a tuple
-        
+
         :return tuple: tuple containing noise configuration
          and mission duration
         """
-        return tuple(k.strip() for k in input_string[1:-1].split(','))
+        return tuple(k.strip() for k in input_string[1:-1].split(","))
